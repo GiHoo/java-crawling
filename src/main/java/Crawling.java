22bfe6efc5648c4acf906e8fa812d1fd7d826a31
kimnamgyu <lethargic.hood@gmail.com>
2024-02-08 18:58:53 +0900

    feat: 링크를 입력받아 웹 페이지 전체를 크롤링 해 html 파일을 생성

--- src/main/java/Crawling.java
+++ src/main/java/Crawling.java
@@ -1,20 +1,20 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Crawling {
-    public static void main(String[] args) {
-        // 크롤링할 웹 페이지 URL
-        String url = "https://www.acmicpc.net/problem/12772";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String url = br.readLine();
+        Document doc = Jsoup.connect(url).get();
 
         try {
-            // Jsoup을 사용하여 웹 페이지의 HTML을 가져옴
-            Document doc = Jsoup.connect(url).get();
-
             // .html 파일 생성
             File htmlFile = new File("output.html");
             FileWriter fileWriter = new FileWriter(htmlFile);
@@ -31,6 +31,5 @@ public class Crawling {
 }
 
 /**
- * 크롤링한 이 html 파일을 Json 형식으로 담아서 프론트로 전달.
- * 아마 escape 문자나 처리할 것이 있을 것으로 보임
+ * 크롤링한 이 html 파일을 Json 형식으로 담아서 프론트로 전달. 아마 escape 문자나 처리할 것이 있을 것으로 보임
  */
