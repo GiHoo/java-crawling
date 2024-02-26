5887ff6d189d9435b063b8f5940426f3ced74399
kimnamgyu <lethargic.hood@gmail.com>
2024-02-26 14:28:44 +0900

    feat: 백준 결과 테이블에서 맞았습니다와 틀렸습니다 parsing

--- src/main/java/CrawlingTemp.java
+++ src/main/java/CrawlingTemp.java
@@ -3,32 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 public class CrawlingTemp {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String url = br.readLine();
-        Document doc = Jsoup.connect(url).get();
+        String problemNumber = "2468";
+        String userBojId = "gupeng_0428";
 
-        System.out.println(doc.title());
-        System.out.println("문제 설명: " + doc.body().getElementById("problem_description").text());
+        // 가져오기
+        String prefix = "https://www.acmicpc.net/status?";
+        prefix += "problem_id=" + problemNumber;
+        prefix += "&";
+        prefix += "user_id=" + userBojId;
 
-        System.out.println();
+        System.out.println(prefix);
 
-        System.out.println("입력: " + doc.body().getElementById("problem_input").text());
-        System.out.println("출력: " + doc.body().getElementById("problem_output").text());
+        try {
+            // Jsoup을 사용하여 웹페이지 가져오기
+            Document doc = Jsoup.connect(prefix).get();
 
-        System.out.println();
+            // 해당 HTML 부분 선택하기
+            Element table = doc.selectFirst("#status-table");
 
-        System.out.println("예제 입력 1:");
-        System.out.println(doc.body().getElementById("sample-input-1").text());
+            // 테이블 내부의 첫 번째 행 가져오기
+            Element firstRow = table.selectFirst("td.result");
 
-        System.out.println();
+            String result = firstRow.selectFirst("span.result-text").text();
 
-        System.out.println("예제 출력 1:");
-        System.out.println(doc.body().getElementById("sample-output-1").text());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
